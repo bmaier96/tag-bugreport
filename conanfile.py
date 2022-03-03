2f27048d83d0a1d799598aa058f7ca937e3c22c2
Benjamin Maier <Benjamin.Maier@hexagon.com>
2022-03-03 14:28:50 +0100

    fix version

--- conanfile.py
+++ conanfile.py
@@ -11,11 +11,11 @@ class SubprocessConan(ConanFile):
     topics = None
     build_policy = "missing"
     no_copy_source = True
-    version = 1.0
+    version = "1.0"
 
     def source(self):
         git = tools.Git()
-        git.clone("https://github.com/bmaier96/tag-bugreport.git", "master")
+        git.clone("https://github.com/bmaier96/tag-bugreport.git", "main")
 
     def package(self):
         self.copy("*.h", dst=self.package_folder, src=self.source_folder, symlinks=True)
